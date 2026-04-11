cdece14cadf2bd591e93e852eebb4bc4a6b8995c
sofieseime <sofie.molvig@hotmail.no>
2026-04-11 18:54:31 +0200

    Create products.js

---
+++ products.js
@@ -0,0 +1,29 @@
+const products = [
+  {
+    id: 1,
+    name: "Pink Princess Dress",
+    price: 899,
+    image: "1.png",
+    link: "product1.html",
+    category: "dress",
+    keywords: ["pink", "princess", "dress", "preorder"]
+  },
+  {
+    id: 2,
+    name: "White Dream Dress",
+    price: 949,
+    image: "1.png",
+    link: "product2.html",
+    category: "dress",
+    keywords: ["white", "dream", "dress", "preorder"]
+  },
+  {
+    id: 3,
+    name: "Princess Evening Dress",
+    price: 999,
+    image: "1.png",
+    link: "product3.html",
+    category: "dress",
+    keywords: ["princess", "evening", "dress", "preorder"]
+  }
+];
